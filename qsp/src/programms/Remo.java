958576d1097edc7cf4e7707d885b119e2c24b4dc
SurajQspiders <82817759+SurajQspiders@users.noreply.github.com>
2021-04-19 18:30:46 +0530

    Update Remo.java

--- qsp/src/programms/Remo.java
+++ qsp/src/programms/Remo.java
@@ -5,7 +5,7 @@ package programms;
 public class Remo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hi I am from remo class");
+		System.out.println("Hi I am from remo class and i have undergone some changes in github");
 	}
 
 }
